2633eca4f6abcf8fe7723f5b802d47a96a93aec4
Leticia <166722628+Andrade-Leticia@users.noreply.github.com>
2025-09-24 11:23:36 -0300

    base conhecimento pt1
    primeira parte da modelagem da base de conhecimento

---
+++ base_conhecimento .pl
@@ -0,0 +1,13 @@
+% *** MODELAGEM DA BASE DE RECONHECIMENTO *** - Leticia
+
+%fatos
+trilha(inteligencia_artificial, 'Aplicação e estudo de algoritmos inteligentes, machine learning e visão computacional').
+trilha(desenvolvimento_web, ' Criação de sistemas e aplicações web, front-end e back-end').
+trilha(seguranca_da_informacao, 'Proteção de dados, criptografia e análise de vulnerabilidade de sistemas).
+trilha(ciencia_de_dados, 'Análise e interpretação de grande volume de dados, machine learning').
+trilha(redes_e_infraestrutura, 'Manutenção e configuração de redes, servidores de infraestrutura de sistemas').
+
+perfil(inteligencia_artificial, logica_de_programacao, 4).
+perfil(inteligencia_artificial, matematica_estatistica, 5).
+perfil(desenvolvimento_web, design_visual,3).                                                                                                            
+                                                                                                        
