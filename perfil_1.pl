cd5915700c6c62cbe3ea92eb4a33f0b41652c061
Brenda Barbosa <151410709+brebarbosaa@users.noreply.github.com>
2025-09-28 20:20:59 -0300

    Adição de arquivos de teste

---
+++ perfil_1.pl
@@ -0,0 +1,11 @@
+% Perfil: Aluno com inclinação para Segurança da Informação
+resposta(1, n).
+resposta(2, s).
+resposta(3, s).
+resposta(4, n).
+resposta(5, n).
+resposta(6, n).
+resposta(7, s).
+resposta(8, s).
+resposta(9, s).
+resposta(10, n).
---
+++ perfil_2.pl
@@ -0,0 +1,11 @@
+% Perfil: Aluno com inclinação para Desenvolvimento Web
+resposta(1, n).
+resposta(2, n).
+resposta(3, s).
+resposta(4, s).
+resposta(5, s).
+resposta(6, s).
+resposta(7, s).
+resposta(8, n).
+resposta(9, n).
+resposta(10, n).
